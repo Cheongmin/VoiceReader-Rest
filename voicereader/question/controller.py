2988983b271e3f315b012af94b67140b46660c32
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-11-11 22:05:02 +0900

    Add prototype handlers

--- voicereader/question/controller.py
+++ voicereader/question/controller.py
@@ -3,6 +3,21 @@ from flask import Blueprint
 question_api = Blueprint('questions', __name__)
 
 
-@question_api.route('/')
-def index():
+@question_api.route('/', methods=['GET'])
+def fetch_all():
     return 'Question api'
+
+
+@question_api.route('/<question_id>', methods=['GET'])
+def fetch_by_id(question_id):
+    pass
+
+
+@question_api.route('/', methods=['POST'])
+def add():
+    pass
+
+
+@question_api.route('/<question_id>', methods=['DELETE'])
+def remove(question_id):
+    pass
--- voicereader/user/controller.py
+++ voicereader/user/controller.py
@@ -3,6 +3,26 @@ from flask import Blueprint
 user_api = Blueprint('users', __name__)
 
 
-@user_api.route('/')
-def index():
-    return 'Users Api'
+@user_api.route('/<user_id>', methods=['GET'])
+def fetch_by_id(user_id):
+    pass
+
+
+@user_api.route('/', methods=['POST'])
+def add():
+    pass
+
+
+@user_api.route('/<user_id>/photo', methods=['POST'])
+def uploadPhoto(user_id):
+    pass
+
+
+@user_api.route('/<user_id>', methods=['PUT'])
+def update(user_id):
+    pass
+
+
+@user_api.route('/<user_id>', methods=["DELETE"])
+def remove():
+    pass
