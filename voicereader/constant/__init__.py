4422826e2ea02d2f4d966530f46a2d96f1dfd8dd
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-11-21 23:03:57 +0900

    Refactor question controller

--- voicereader/constant/__init__.py
+++ voicereader/constant/__init__.py
@@ -3,6 +3,7 @@ from flask import jsonify
 MSG_NOT_EQUAL_IDENTITY = 'not equal between request identity and token identity'
 MSG_NOT_FOUND_ELEMENT = "Can't find element"
 MSG_INVALID_JSON = "Can't convert json"
+MSG_NOT_CONTAIN_SOUND = "Can't find sound file"
 
 
 def msg_json(msg):
--- voicereader/question/controller.py
+++ voicereader/question/controller.py
@@ -3,9 +3,14 @@ import ast
 import datetime
 import time
 import os
-from flask import Blueprint, request, jsonify, send_from_directory, url_for
+
+from flask import Blueprint, request, jsonify, send_from_directory
 from flask_pymongo import PyMongo
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from bson import ObjectId
+
+from voicereader.constant import action_result, msg_json
+from voicereader.constant import MSG_NOT_FOUND_ELEMENT, MSG_NOT_CONTAIN_SOUND
 
 _question_api = Blueprint('questions', __name__)
 mongo = PyMongo()
@@ -20,6 +25,7 @@ def get_question_api(app):
 
 
 @_question_api.route('', methods=['GET'])
+@jwt_required
 def fetch_all():
     try:
         offset = request.args.get("offset", 0)
@@ -32,41 +38,41 @@ def fetch_all():
         for record in records_fetched:
             result.append(record)
 
-        return jsonify(result), 200
+        return action_result.ok(jsonify(result))
     except Exception as ex:
-        print(ex)
-        return "", 500
+        return action_result.internal_server_error(msg_json(str(ex)))
 
 
 @_question_api.route('/<question_id>', methods=['GET'])
+@jwt_required
 def fetch_by_id(question_id):
     """ Function to fetch the questions. """
     try:
         # Fetch all the record(s)
         records_fetched = mongo.db.questions.find_one({"_id": ObjectId(question_id)})
+        if records_fetched is None:
+            return action_result.not_found(msg_json(MSG_NOT_FOUND_ELEMENT))
 
-        if records_fetched is not None:
-            return jsonify(records_fetched), 200
-        else:
-            return "", 404
+        return action_result.ok(jsonify(records_fetched))
     except Exception as ex:
         # Error while trying to fetch the resource
         # Add message for debugging purpose
-        print(ex)
-        return "", 500
+        return action_result.internal_server_error(msg_json(str(ex)))
 
 
 @_question_api.route('/sound/<path:filename>', methods=['GET', 'POST'])
+@jwt_required
 def fetch_sound_file(filename):
     return send_from_directory(os.path.abspath(UPLOAD_FOLDER), filename, as_attachment=True)
 
 
 @_question_api.route('', methods=['POST'])
+@jwt_required
 def add():
     """ Function to create new question. """
     try:
         if 'sound' not in request.files:
-            return "", 400
+            return action_result.bad_request(msg_json(MSG_NOT_CONTAIN_SOUND))
 
         # Create new question
         try:
@@ -75,10 +81,10 @@ def add():
         except Exception as ex:
             # Bad request as request body is not available
             # Add message for debugging purpose
-            print(ex)
-            return "", 400
+            return action_result.bad_request(msg_json(str(ex)))
 
         body['_id'] = ObjectId()
+        body["writer_id"] = ObjectId(get_jwt_identity())
         body["created_date"] = time.mktime(datetime.datetime.utcnow().timetuple())
 
         sound_file = request.files['sound']
@@ -91,30 +97,30 @@ def add():
 
         record_created = mongo.db.questions.insert(body)
 
-        return jsonify(body), 201
+        return action_result.created(jsonify(body))
     except Exception as ex:
         # Error while trying to create the resource
         # Add message for debugging purpose
-        print(ex)
-        return "", 500
+        return action_result.internal_server_error(msg_json(str(ex)))
 
 
 @_question_api.route('/<question_id>', methods=['DELETE'])
+@jwt_required
 def remove(question_id):
     """ Function to remove the question. """
     try:
         # Delete the question matched
-        record_deleted = mongo.db.questions.delete_one({"_id": ObjectId(question_id)})
+        record_deleted = mongo.db.questions.delete_one({"$and": [{"_id": ObjectId(question_id)},
+                                                                 {"writer_id": ObjectId(get_jwt_identity())}]})
 
         # Prepare the response
         if record_deleted.deleted_count > 0:
             # We return 204 No Content to imply resource updated successfully without returning
             # the deleted entity.
-            return "", 204
+            return action_result.no_content()
         else:
             # Entity not found, perhaps already deleted, return 404
-            return "", 404
+            return action_result.not_found(msg_json(MSG_NOT_FOUND_ELEMENT))
     except Exception as ex:
         # Something went wrong server side, so return Internal Server Error.
-        print(ex)
-        return "", 500
+        return action_result.internal_server_error(msg_json(str(ex)))
