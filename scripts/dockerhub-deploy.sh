76ffe34b948d5ef9a7a0eae771f44abdb6656487
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-11-26 15:47:44 +0900

    Increase security rule

--- scripts/dockerhub-deploy.sh
+++ scripts/dockerhub-deploy.sh
@@ -1,7 +1,7 @@
-docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+docker login -u ${DOCKER_USERNAME} --password-stdin ${DOCKER_PASSWORD}
 
 docker build -t gyuhwankim/voicereader-rest:0.0.1 .
-docker tag gyuhwankim/voicereader-rest:0.0.1 outsideris/demo:latest
+docker tag gyuhwankim/voicereader-rest:0.0.1 gyuhwankim/voicereader-rest:latest
 
 docker push gyuhwankim/voicereader-rest:0.0.1
 docker push gyuhwankim/voicereader-rest:latest
