8707987e3d54827796bc10b6f641e362824b073c
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-11-26 15:10:36 +0900

    Rename deploy.sh to docker-deploy.sh

---
+++ scripts/dockerhub-deploy.sh
@@ -0,0 +1,7 @@
+docker login -u DOCKER_USERNAME -p DOCKER_PASSWORD
+
+docker build -t gyuhwankim/voicereader-rest:0.0.1 .
+docker tag gyuhwankim/voicereader-rest:0.0.1 outsideris/demo:latest
+
+docker push gyuhwankim/voicereader-rest:0.0.1
+docker push gyuhwankim/voicereader-rest:latest
