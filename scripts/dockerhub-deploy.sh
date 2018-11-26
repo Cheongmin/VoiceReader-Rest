57f35c05256716c5cedf759c4aff4c96619a4f74
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-11-26 15:36:33 +0900

    Fix using env variables

--- scripts/dockerhub-deploy.sh
+++ scripts/dockerhub-deploy.sh
@@ -1,4 +1,4 @@
-docker login -u DOCKER_USERNAME -p DOCKER_PASSWORD
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 
 docker build -t gyuhwankim/voicereader-rest:0.0.1 .
 docker tag gyuhwankim/voicereader-rest:0.0.1 outsideris/demo:latest
