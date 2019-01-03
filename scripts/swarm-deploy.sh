1ab4a0d5e8cdf670ae523a3e8663b91f5967a994
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-01-03 11:36:23 +0900

    Execute process that remove untagged image and stopped container

--- scripts/swarm-deploy.sh
+++ scripts/swarm-deploy.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 deploy_stack_name=voicereader-rest
 docker_user_name=gyuhwankim
 api_image_name=voicereader-rest
@@ -6,6 +8,9 @@ if sudo docker stack ls --format '{{.Name}}' | grep -Eq "^${deploy_stack_name}\$
   docker pull ${docker_user_name}/${api_image_name}:latest
 
   docker service update --image ${docker_user_name}/${api_image_name} ${deploy_stack_name}_api
+
+  docker rm $(docker ps -aq)
+  docker rmi $(docker images -q --filter "dangling=true")
 else
   docker stack deploy -c stack-compose.yml ${deploy_stack_name}
 fi
