dcd09714fe87bf85cb2618206770fc41253bf0dd
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-01-14 17:19:12 +0900

    Add picture field

--- voicereader/api/v1/user/schema.py
+++ voicereader/api/v1/user/schema.py
@@ -3,11 +3,12 @@ from flask_restplus import fields
 
 def user_schema(api):
     return api.model('User', {
-        '_id': fields.String(description='Id of user', example='5c38b8df18283838d53dfe37'),
-        'display_name': fields.String(description='The Nickname', example='Lion'),
-        'created_date': fields.Arbitrary(description='user created date', example=1547188815),
-        'email': fields.String(description='email of user'),
-        'fcm_uid': fields.String(description='unique id from firebase auth'),
+        '_id': fields.String(description='User ID', example='5c38b8df18283838d53dfe37'),
+        'display_name': fields.String(description='Nickname', example='Lion'),
+        'email': fields.String(description='Email of user', example='lion@example.com'),
+        'fcm_uid': fields.String(description='Unique id from firebase auth'),
+        'picture': fields.String(description='URL of profile picture'),
+        'created_date': fields.Integer(description='user created date', example=1547188815),
     })
 
 
