9bd20137cd8896cb04c68909ac3192edf9db115e
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-01-16 14:08:33 +0900

    Support docker image versioning

--- scripts/swarm-deploy.sh
+++ scripts/swarm-deploy.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-deploy_stack_name=voicereader-rest
-docker_user_name=gyuhwankim
-api_image_name=voicereader-rest
-
-if sudo docker stack ls --format '{{.Name}}' | grep -Eq "^${deploy_stack_name}\$"; then
-  docker pull ${docker_user_name}/${api_image_name}:latest
-  docker service update --image ${docker_user_name}/${api_image_name} ${deploy_stack_name}_api
+if sudo docker stack ls --format '{{.Name}}' | grep -Eq "^${DEPLOY_STACK_NAME}\$"; then
+  docker pull ${DOCKER_USERNAME}/${DOCKER_IMAGE_NAME}:latest
+  docker service update --image ${DOCKER_USERNAME}/${DOCKER_IMAGE_NAME} ${DEPLOY_STACK_NAME}_${CONTAINER_API_NAME}
 else
-  docker stack deploy -c stack-compose.yml ${deploy_stack_name}
+  docker stack deploy -c stack-compose.yml ${DEPLOY_STACK_NAME}
 fi
 
 # docker rm $(docker ps -aq --filter "status=exited")
