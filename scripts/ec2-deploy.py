92b34a6a0d02952a63f725eb8bb5c10bb4aef243
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-01-21 18:00:56 +0900

    Set travis env on ec2

--- scripts/ec2-deploy.py
+++ scripts/ec2-deploy.py
@@ -11,6 +11,8 @@ src = 'voicereader-rest/'
 
 
 def deploy():
+    set_env()
+
     run('mkdir -p {}'.format(src))
 
     put('nginx.conf', src)
@@ -19,3 +21,15 @@ def deploy():
 
     with cd(src):
         run('bash swarm-deploy.sh')
+
+
+def set_env():
+    deploy_stack_name = os.getenv('DEPLOY_STACK_NAME')
+    docker_user_name = os.getenv('DOCKER_USERNAME')
+    docker_image_name = os.getenv('DOCKER_IMAGE')
+    container_api_name = os.getenv('CONTAINER_API_NAME')
+
+    run('export DEPLOY_STACK_NAME={}'.format(deploy_stack_name))
+    run('export DOCKER_USERNAME={}'.format(docker_user_name))
+    run('export DOCKER_IMAGE_NAME={}'.format(docker_image_name))
+    run('export CONTAINER_API_NAME={}'.format(container_api_name))
