55d880257f5cce3478e2eda4647ab127d9ab8613
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-01-27 01:00:01 +0900

    Add test_undefined_model_encode test

--- tests/test_json_encoder.py
+++ tests/test_json_encoder.py
@@ -1,13 +1,25 @@
+from unittest import TestCase
 from bson import ObjectId
 from voicereader.extensions.json_encoder import JSONEncoder
 
 
-def test_object_id_encode():
-    expected = '5c38b8df18283838d53dfe37'
-    object_id = ObjectId(expected)
+class JSONEncoderTests(TestCase):
+    def setUp(self):
+        self.json_encoder = JSONEncoder()
 
-    actual = JSONEncoder().default(object_id)
+    def test_object_id_encode(self):
+        expected = '5c38b8df18283838d53dfe37'
+        object_id = ObjectId(expected)
 
-    assert expected == actual
+        actual = self.json_encoder.default(object_id)
 
+        assert expected == actual
 
+    def test_undefined_model_encode(self):
+        class Undefined:
+            pass
+
+        undefined = Undefined()
+
+        with self.assertRaises(TypeError):
+            self.json_encoder.default(undefined)
