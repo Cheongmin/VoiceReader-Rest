f200446361e3159945590f7a7f4389cb2dd968e7
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-01-27 22:02:41 +0900

    Fix test_fetch_file_exists_file method

--- voicereader/services/local_storage.py
+++ voicereader/services/local_storage.py
@@ -1,5 +1,4 @@
 import os
-from flask import send_from_directory
 
 
 class LocalStorage:
@@ -12,7 +11,8 @@ class LocalStorage:
             os.makedirs(self.upload_path)
 
     def fetch_file(self, file_name):
-        return send_from_directory(os.path.abspath(self.upload_path), file_name)
+        return open(os.path.join(self.upload_path, file_name), 'rb').read()
+        # return send_from_directory(os.path.abspath(self.upload_path), file_name)
 
     def upload_file(self, file, acl="public-read"):
         if file is None:
