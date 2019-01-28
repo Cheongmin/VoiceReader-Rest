b70fa02e6b4992a1b98b2ad4955b76059adc18c3
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-01-28 10:31:47 +0900

    Add unittests of S3Storage and refactor code

--- voicereader/services/s3_storage.py
+++ voicereader/services/s3_storage.py
@@ -1,36 +1,60 @@
+import os
 import boto3
-from flask import make_response
 
 
 class S3Storage:
-    s3 = None
-    bucket_name = None
+    _bucket_name = None
+    _s3 = None
 
     def init_app(self, app):
-        self.bucket_name = app.config['S3_BUCKET_NAME']
-
-        self.s3 = boto3.client(
+        self._bucket_name = app.config['S3_BUCKET_NAME']
+        self._s3 = boto3.client(
             "s3",
             region_name=app.config['S3_REGION'],
             aws_access_key_id=app.config['S3_KEY'],
             aws_secret_access_key=app.config['S3_SECRET']
         )
 
-    def fetch_file(self, prefix, file_name, as_attachment=False):
-        key = '{}{}'.format(prefix, file_name)
+    def fetch_file(self, resource, filename):
+        if self._s3 is None:
+            raise TypeError(self._s3)
 
-        file = self.s3.get_object(Bucket=self.bucket_name, Key=key)
-        response = make_response(file['Body'].read())
-        response.headers['Content-Type'] = file['ContentType']
+        if self._bucket_name is None:
+            raise TypeError(self._bucket_name)
 
-        if as_attachment:
-            response.headers['Content-Disposition'] = 'attachment'
+        if resource is None:
+            raise ValueError(resource)
 
-        return response
+        if filename is None:
+            raise ValueError
 
-    def upload_file(self, prefix, file):
-        key = '{}{}'.format(prefix, file.filename)
+        key = os.path.join(resource, filename)
 
-        self.s3.upload_fileobj(file, self.bucket_name, Key=key, ExtraArgs={
+        file = self._s3.get_object(Bucket=self._bucket_name, Key=key)
+
+        return file['Body'].read()
+        #response.headers['Content-Type'] = file['ContentType']
+
+        #if as_attachment:
+            #response.headers['Content-Disposition'] = 'attachment'
+
+        #return response
+
+    def upload_file(self, resource, file):
+        if self._s3 is None:
+            raise TypeError(self._s3)
+
+        if self._bucket_name is None:
+            raise TypeError(self._bucket_name)
+
+        if resource is None:
+            raise ValueError(resource)
+
+        if file is None:
+            raise ValueError(file)
+
+        key = os.path.join(resource, file.filename)
+
+        self._s3.upload_fileobj(file, self._bucket_name, Key=key, ExtraArgs={
             "ContentType": file.content_type,
         })
