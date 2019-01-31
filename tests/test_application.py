b702a42b2d80c9d73b262b2d74a54319fd7a4ca7
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-01-31 17:26:28 +0900

    Fix test_create broken

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,7 +1,9 @@
-from voicereader import application
+from voicereader import application, startup
 
 
-def test_create():
+def test_create(monkeypatch):
+    monkeypatch.setattr(startup, 'configure_middleware', lambda x: None)
+
     app = application.create()
 
     assert app is not None
