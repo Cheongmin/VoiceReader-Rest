441730a3a91057c2119dd8cc140ad644d9fa9c0b
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-02-03 19:50:05 +0900

    Fix broken test

--- voicereader/api_v1/blueprint.py
+++ voicereader/api_v1/blueprint.py
@@ -6,8 +6,6 @@ from .user.controller import api as user_namespace
 from .question.controller import api as question_namespace
 from .answer.controller import api as answer_namespace
 
-from .middlewares import jwt
-
 blueprint = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 api = Api(blueprint, version='v1', title='VoiceReader API',
           description='This api provide data that need voicereader app')
@@ -16,5 +14,3 @@ api.add_namespace(auth_namespace, path='/oauth2')
 api.add_namespace(user_namespace, path='/users')
 api.add_namespace(question_namespace, path='/questions')
 api.add_namespace(answer_namespace, path='/questions')
-
-jwt.init_app(api)
