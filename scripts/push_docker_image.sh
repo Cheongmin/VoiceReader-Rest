4acc535899a7a37ba3dbea9fc0d8ac5dfe31dd16
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-02-08 00:42:17 +0900

    Add scripts about docker supporting

---
+++ scripts/push_docker_image.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Require envvars
+# DOCKER_USERNAME
+# DOCKER_PASSWORD
+# DOCKER_IMAGE_NAME
+
+if [[ -z $1 ]]; then
+    echo "Can't get tag of image"
+    exit 1
+fi
+
+IMAGE_TAG=$1
+
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+
+docker push ${DOCKER_USERNAME}/${DOCKER_IMAGE_NAME}:${IMAGE_TAG}
+docker push ${DOCKER_USERNAME}/${DOCKER_IMAGE_NAME}:latest
