93a6844ca038db12a50ab3a4879bec8a932887a3
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-02-08 12:10:51 +0900

    Add error handling

--- scripts/push_docker_image.sh
+++ scripts/push_docker_image.sh
@@ -12,6 +12,10 @@ fi
 IMAGE_TAG=$1
 
 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+if [[ $? != 0 ]]; then
+    echo "Can't dockerhub login"
+    exit 2
+fi
 
 docker push ${DOCKER_USERNAME}/${DOCKER_IMAGE_NAME}:${IMAGE_TAG}
 docker push ${DOCKER_USERNAME}/${DOCKER_IMAGE_NAME}:latest
